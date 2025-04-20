03726919d58d80fcc1fb8242e7b598f72b95d4d5
Jyotsna Thakur <j.thakur2608@gmail.com>
2025-04-20 22:37:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration settings
+thresholds = {
+    "blink_rate": 0.2,
+    "cognitive_load": 0.5,
+}
+models = {
+    "knn": "data/models/knn_model.pkl",
+    "random_forest": "data/models/random_forest.pkl",
+}
